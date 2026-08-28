14f78787975a047088c903cf3fa34bb1807074aa
kyroshbrand <kyroshbrand@gmail.com>
2026-08-28 12:25:47 +0530

    Delete old Next.js app files

--- app/(admin)/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div style={{ minHeight: "100vh", background: "#050008" }}>
-      {children}
-    </div>
-  );
-}
